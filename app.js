510a8bd0d38abfa265c80a0fb86bbd79bf4bc05a
Daniel Herrero <danielherrerohernando@gmail.com>
2021-05-24 20:33:16 +0200

    feat: adding user endpoints

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const fs = require('fs');
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const rashaRouter = require('./routes/users');
 
 const app = express();
 
@@ -15,8 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+app.use('/users', rashaRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
-var router = express.Router();
+var angelrouter = express.Router();
+
+const fs = require('fs')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+angelrouter.get('/:userName', function(req, res) {
+  const data = fs.readFileSync(`./database/${req.params.userName}.json`, {encoding: 'utf-8'})
+  const dataParsed = JSON.parse(data)
+  console.log('el tipo de dato de lo que hemos leido es: ',typeof data);
+  console.log('el tipo de dato de lo que hemos leido y parseado es: ',typeof dataParsed);
+  res.json(dataParsed)
 });
 
-module.exports = router;
+angelrouter.post('/', function(req, res) {
+  const {user, task} = req.body;
+  fs.writeFileSync(`./database/${user}.json`, JSON.stringify([task]))
+  res.json({success: 'Data correctamente insertado'});
+});
+
+module.exports = angelrouter;
